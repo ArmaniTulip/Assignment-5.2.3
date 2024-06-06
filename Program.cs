8686b971914caba68ecb97ed34767a00ba159454
Garrett <Garrett@YUP>
2024-06-06 11:46:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment_5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BackWardsCount(10);
+        }
+
+        static void BackWardsCount(int n)
+        {
+            if (n > 0 )
+            {
+                Console.WriteLine(n);
+                BackWardsCount(n - 1);
+            }
+            
+        }
+    }
+}
